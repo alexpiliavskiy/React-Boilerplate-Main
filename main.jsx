2c39d86f342319e0d18d21634ba6356b3cbb9819
alexp <alex.pilyavskiy03@gmail.com>
2025-11-11 12:30:07 +0200

    first commit

--- main.jsx
+++ main.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "../src/styles/index.scss";
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")).render(<App />);
+
